246421068bd923ce3afbd8314bf7c9b4c73c05bf
hp <engrziaurrehman.kicsit@gmail.com>
2022-12-12 14:38:40 +0500

    First Commit

---
+++ Sorting Techniques/BubbleSort.py
@@ -0,0 +1,35 @@
+import time
+import psutil, os
+
+# generating a random number
+import random
+
+n = 1000
+list = []
+
+for i in range(0, n):   # for i in range(n,0,-1):  for reverse sorted list of number
+    list.append((random.randint(1,1000)))
+    # list.append(i)
+
+# print(f"Before Sorting: {list}")
+
+start = time.time()
+for i in range(0,n-1):
+    
+    for j in range(0,n-i-1):
+        
+        if list[j] > list[j+1]:
+            
+            temp = list[j]
+            list[j] = list[j+1]
+            list[j+1] = temp
+
+cpu1 = psutil.cpu_percent(interval=1)  
+
+end = time.time()
+
+cpu2 = psutil.cpu_percent(interval=1)  
+# print(f"After Sorting: {list}")
+
+print(f"Time Take: {end-start} seconds")
+print(f"CPU utilization: {cpu1}, {cpu2}")
